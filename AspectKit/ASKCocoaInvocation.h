351be7ec147c15df1f322d7cd05ecf9addf25ee1
MO-AI <moai.motodera@gmail.com>
2015-09-23 18:36:13 +0900

    Add ASKCocoaInvocation.swift

---
+++ AspectKit/ASKCocoaInvocation.h
@@ -0,0 +1,13 @@
+//
+//  ASKCocoaInvocation.h
+//  AspectKit
+//
+//  Created by Hiromi Motodera on 9/23/15.
+//  Copyright (c) 2015 Hiromi Motodera. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ASKCocoaInvocation : NSObject
+
+@end
---
+++ AspectKit/ASKCocoaInvocation.m
@@ -0,0 +1,13 @@
+//
+//  ASKCocoaInvocation.m
+//  AspectKit
+//
+//  Created by Hiromi Motodera on 9/23/15.
+//  Copyright (c) 2015 Hiromi Motodera. All rights reserved.
+//
+
+#import "ASKCocoaInvocation.h"
+
+@implementation ASKCocoaInvocation
+
+@end
